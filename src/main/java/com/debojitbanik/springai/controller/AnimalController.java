45a9c223b649beda711c5ea2fc01cdc3b3d6d17a
Debojit Kangsa Banik <debojitkangsabanik@gmail.com>
2025-08-24 23:20:51 +0530

    spring openai and groq token

--- src/main/java/com/debojitbanik/springai/controller/AnimalController.java
+++ src/main/java/com/debojitbanik/springai/controller/AnimalController.java
@@ -2,21 +2,29 @@ package com.debojitbanik.springai.controller;
 
 import com.debojitbanik.springai.model.Animal;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/animals")
 public class AnimalController {
 
     private final ChatClient chatClient;
-    public AnimalController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+
+    public AnimalController(ChatClient.Builder chatClientBuilder, ChatMemory chatMemory) {
+        this.chatClient = chatClientBuilder
+                .defaultAdvisors(PromptChatMemoryAdvisor.builder(chatMemory).build())
+                .build();
     }
 
     @GetMapping
@@ -27,6 +35,18 @@ public class AnimalController {
                 """);
         return this.chatClient.prompt(pt.create())
                 .call()
-                .entity(new ParameterizedTypeReference<>() {});
+                .entity(new ParameterizedTypeReference<>() {
+                });
+    }
+
+    @GetMapping("/{id}")
+    Animal findById(@PathVariable String id) {
+        PromptTemplate pt = new PromptTemplate("""
+                Find and return the object with id {id} in a current list of animals.
+                """);
+        Prompt p = pt.create(Map.of("id", id));
+        return this.chatClient.prompt(p)
+                .call()
+                .entity(Animal.class);
     }
 }
